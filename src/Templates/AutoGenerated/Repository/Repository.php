e65f9345544c29c6c2b151be529ff0d36673526b
Zazalt <liviu+zazalt+github.com@sindla.com>
2016-12-27 20:29:13 +0200

    The cake is a lie.

--- src/Templates/AutoGenerated/Repository/Repository.php
+++ src/Templates/AutoGenerated/Repository/Repository.php
@@ -6,6 +6,59 @@ class ___CLASS___ ##extends##
 {
     ##construct##
 
+    ####################################################################################################################
+    ###   CRUD operations   ############################################################################################
+
+    /**
+     * Create / Insert
+     * Usage example:
+     *      $Animal = new Animal();
+     *      $Animal->setColor('red');
+     *      $Animal->setSize('small');
+     *      $Animal->setName('Mr. Shrimp');
+     *      $Animal->create();
+     */
+    public function create()
+    {
+        #create()#
+    }
+
+    /**
+     * Read / Select one by primary key
+     */
+    public function __TODO__read()
+    {
+        #read()#
+    }
+
+    /**
+     * Update
+     */
+    public function __TODO__update()
+    {
+        #update()#
+    }
+
+    /**
+     * Delete
+     */
+    public function __TODO__delete()
+    {
+        #delete()#
+    }
+
+    ####################################################################################################################
+
+    public function getOne($what = '*', $where = [], $orderBy = [], $limitOffset = [])
+    {
+        return parent::getOne($what = '*', $where = [], $orderBy = [], $limitOffset = []);
+    }
+
+    public function getAll($what = '*', $where = [], $orderBy = [], $limitOffset = [])
+    {
+        return parent::getOne($what = '*', $where = [], $orderBy = [], $limitOffset = []);
+    }
+
     /**
      * @param array $content - the array before INSERT query is executed
      * @param array $result - the array AFTER the insert query is executed
@@ -15,29 +68,5 @@ class ___CLASS___ ##extends##
     {
         // Can compare $content['?'] with $result['?']
         return $result;
-    }
-
-    /**
-     * Insert or update
-     */
-    public function __TODO__save()
-    {
-
-    }
-
-    /**
-     * Update a DB entry
-     */
-    public function __TODO__update()
-    {
-
-    }
-
-    /**
-     * Delete a DB entry
-     */
-    public function __TODO__delete()
-    {
-
     }
 }
